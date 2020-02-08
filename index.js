636926060c1e7816af01c1a6e773494ed99cb485
Nick Algarra <algarran24@gmail.com>
2020-02-07 19:32:51 -0600

    installed package

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const questions = [
-  
-];
+const fs = require("fs");
+const fs = require("inquirer")
+
+// const questions = [];
+
+inquirer userPrompt(){
+    .prompt([
+        What is your favorite color ?
+    ])
+}
+    
 
 function writeToFile(generateHTML.js, data) {
- 
+
 }
 
 function init() {
 
-init();
+    init();
